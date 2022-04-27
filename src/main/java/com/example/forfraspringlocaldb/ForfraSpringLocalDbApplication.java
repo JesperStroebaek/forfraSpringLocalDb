ac1423f41bf84bd93541549d4487382109fd8455
Jesper <jesp73g2@stud.kea.dk>
2022-04-27 09:50:50 +0200

    Initial commit

---
+++ src/main/java/com/example/forfraspringlocaldb/ForfraSpringLocalDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.forfraspringlocaldb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForfraSpringLocalDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForfraSpringLocalDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/forfraspringlocaldb/ForfraSpringLocalDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forfraspringlocaldb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForfraSpringLocalDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
