897d55cc07dd6c4769ff538fe5cd3a97647da4c0
Jesper <jesp73g2@stud.kea.dk>
2022-04-27 10:19:19 +0200

    Cars klassen er lavet.

--- src/main/java/com/example/forfraspringlocaldb/controller/FrontController.java
+++ src/main/java/com/example/forfraspringlocaldb/controller/FrontController.java
@@ -1,4 +1,4 @@
-package com.example.forfraspringlocaldb;
+package com.example.forfraspringlocaldb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
